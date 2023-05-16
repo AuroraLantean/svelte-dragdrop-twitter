0eaa533711ed3d2c4aa81adbdbc707bf9eec4479
AuroraLantean <raymondlieu.crypto@runbox.com>
2023-05-16 16:51:54 +0800

    add task update()

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -5,15 +5,14 @@
 </script>
 
 <div class="p-10 h-full">
-
   <div class="text-white text-2xl mb-6">Some List</div>
   <button class="text-xl mb-3 text-white font-bold cursor-pointer hover:underline flex items-start">
     + Add List
   </button>
   <div class="flex-it h-full">
     <div class="flex-it flex-row rounded-xl h-full">
-      {#each $taskListStore as list (list.id)}
-        <TaskList listName={list.text} tasks={list.items} />
+      {#each $taskListStore as list, listIdx (list.id)}
+        <TaskList listName={list.text} tasks={list.items} {listIdx} />
       {/each}
     </div>
   </div>
