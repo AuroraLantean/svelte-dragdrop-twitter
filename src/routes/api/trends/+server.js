b2749ae555465fd51a9db31a9ee338719c9ae685
AuroraLantean <raymondlieu.crypto@runbox.com>
2023-05-20 16:35:42 +0800

    complete static to dynamic data

--- src/routes/api/trends/+server.js
+++ src/routes/api/trends/+server.js
@@ -1,5 +1,9 @@
-<script>
+
+import { json } from "@sveltejs/kit";
+
+export function GET() {
   const randomize = () => Math.floor(Math.random() * 500);
+
   const trends = [
     {
       category: "Sports",
@@ -31,24 +35,7 @@
       content: "Peter Jackson as the director of new Lotr",
       glideCount: randomize()
     },
-  ]
-  console.log(trends);
-</script>
+  ];
 
-
-<div class="bg-gray-800 overflow-hidden flex-it rounded-2xl">
-  <div class="flex-it p-4">
-    <span class="text-xl font-bold">Trends</span>
-  </div>
-
-  {#each trends as trend }
-    <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-      <div class="flex-it">
-        <span class="text-gray-400 text-sm">{trend.content}</span>
-        <span class="text-lg font-bold">{trend.category}</span>
-        <span class="text-gray-400 text-sm">{trend.glideCount} glides</span>
-      </div>
-    </div>
-  {/each}
-
-</div>
+  return json(trends);
+}
--- src/routes/task-manager/+page.js
+++ src/routes/task-manager/+page.js
@@ -3,7 +3,7 @@ export const ssr = false;
 
 export function load() {
   // fetch data from API/Database ...
-  console.log("Calling task-manager/load!");
+  //console.log("Calling task-manager/load!");
   return {
     appName: "Task Manager",
     content: "Board"
